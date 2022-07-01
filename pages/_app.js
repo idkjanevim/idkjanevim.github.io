300f638c30653d6565def50b51b0f0eb16464e87
idkjanevim <ondatra@centrum.cz>
2022-07-01 16:22:00 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/style.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></Script>
             <Component {...pageProps} />
         </>
     );
