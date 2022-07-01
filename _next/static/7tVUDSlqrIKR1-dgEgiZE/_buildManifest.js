dcc76de955f598ea2e7a4d14e0066e6f58687b38
idkjanevim <58387601+idkjanevim@users.noreply.github.com>
2022-07-01 17:28:35 +0000

    Deploying to gh-pages from @ idkjanevim/idkjanevim.github.io@0c3b91c86863e534f8d074595a0c3f8e1c31f9c3 🚀

--- _next/static/7tVUDSlqrIKR1-dgEgiZE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/506-a5257c860be85b04.js","static/chunks/pages/index-a8703ef5c574fc73.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/NH5D8MzCwQvBWHLXJWsky/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/506-a5257c860be85b04.js","static/chunks/pages/index-79c4b58f2c2d01f7.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
