6ac8363f6bbd6902c1c9924f4034b56a9654ff14
idkjanevim <ondatra@centrum.cz>
2022-07-01 16:40:26 +0200

    init

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
         loader: 'akamai',
         path: '',
     },
+    basePath: '/idkjanevim.github.io',
+    assetPrefix: '/idkjanevim.github.io/',
 };
 
 module.exports = nextConfig;
