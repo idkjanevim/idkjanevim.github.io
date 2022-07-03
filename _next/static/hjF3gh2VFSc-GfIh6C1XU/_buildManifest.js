2541ceae5fb4b20589c36787d465390db21f2e0c
idkjanevim <58387601+idkjanevim@users.noreply.github.com>
2022-07-03 12:50:29 +0000

    Deploying to gh-pages from @ idkjanevim/idkjanevim.github.io@98db6352c1d22fd3e51daf88a4c5ab03bb7c2e13 🚀

---
+++ _next/static/hjF3gh2VFSc-GfIh6C1XU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/191-4edc2080b6295581.js","static/css/ae4ed9c503fd1e33.css","static/chunks/pages/index-05220c7208cf8336.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
