3d6bac2be3d0405b6d10cf9f975db8731a109111
idkjanevim <ondatra@centrum.cz>
2022-07-03 14:47:40 +0200

    Carousel

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 function Projects() {
     return (
@@ -13,91 +15,113 @@ function Projects() {
                 <h1 className="text-white text-center pb-5">My projects</h1>
                 <h2 className="text-white text-center py-5">Web</h2>
                 <div className="d-flex justify-content-center flex-column flex-md-row">
-                    <div className="card mx-5 border my-5 my-md-0">
-                        <Image
-                            src="/imgs/spejs.png"
-                            height={250}
-                            width={406}
-                            className="card-img-top"
-                        />
-                        <div className="card-body">
-                            <h5 className="card-title">Spejs</h5>
-                            <p className="card-text">
-                                Small website about Nasa and Space. <br /> This
-                                was my first project with Nextjs <br /> and for
-                                same reason a wanted to do <br /> something with
-                                API
-                            </p>
-                            <div className="d-flex justify-content-between">
-                                <a
-                                    href="https://idkjanevim.github.io/spejs-nextjs/"
-                                    className="btn btn-primary"
-                                >
-                                    Go to website
-                                </a>
-                                <a
-                                    href="https://github.com/idkjanevim/spejs-nextjs"
-                                    className="btn btn-primary"
-                                >
-                                    Go to Github page
-                                </a>
+                    <div className="crsel">
+                        <Carousel
+                            emulateTouch={true}
+                            autoPlay={true}
+                            infiniteLoop={true}
+                            interval={5000}
+                        >
+                            <div className="card mx-5 border my-5 my-md-0">
+                                <Image
+                                    src="/imgs/spejs.png"
+                                    height={250}
+                                    width={406}
+                                    className="card-img-top"
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">Spejs</h5>
+                                    <p className="card-text">
+                                        Small website about Nasa and Space.{' '}
+                                        <br /> This was my first project with
+                                        Nextjs <br /> and for same reason a
+                                        wanted to do <br /> something with API
+                                    </p>
+                                    <div className="d-flex justify-content-between">
+                                        <a
+                                            href="https://idkjanevim.github.io/spejs-nextjs/"
+                                            className="btn btn-primary"
+                                        >
+                                            Go to website
+                                        </a>
+                                        <a
+                                            href="https://github.com/idkjanevim/spejs-nextjs"
+                                            className="btn btn-primary"
+                                        >
+                                            Go to Github page
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div className="card mx-5 border my-5 my-md-0">
-                        <Image
-                            src="/imgs/portfolio.png"
-                            height={250}
-                            width={406}
-                            className="card-img-top"
-                        />
-                        <div className="card-body">
-                            <h5 className="card-title">My portfolio website</h5>
-                            <p className="card-text">
-                                My portfolio (this website). <br /> I wanted a
-                                way to showcase my work so I built this
-                            </p>
-                            <div className="d-flex justify-content-between pt-5">
-                                <a href="#" className="btn btn-primary">
-                                    Go to website
-                                </a>
-                                <a
-                                    href="https://github.com/idkjanevim/idkjanevim.github.io"
-                                    className="btn btn-primary"
-                                >
-                                    Go to Github page
-                                </a>
+                            <div className="card mx-5 border my-5 my-md-0">
+                                <Image
+                                    src="/imgs/portfolio.png"
+                                    height={250}
+                                    width={406}
+                                    className="card-img-top"
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        My portfolio website
+                                    </h5>
+                                    <p className="card-text">
+                                        My portfolio (this website). <br /> I
+                                        wanted a way to showcase my work so I
+                                        built this
+                                    </p>
+                                    <div className="d-flex justify-content-between pt-5">
+                                        <a href="#" className="btn btn-primary">
+                                            Go to website
+                                        </a>
+                                        <a
+                                            href="https://github.com/idkjanevim/idkjanevim.github.io"
+                                            className="btn btn-primary"
+                                        >
+                                            Go to Github page
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </Carousel>
                     </div>
                 </div>
                 <h2 className="text-white text-center py-5">Other</h2>
                 <div className="d-flex justify-content-center flex-sm-column flex-lg-row">
-                    <div className="card mx-5 border my-5 my-md-0">
-                        <Image
-                            src="/imgs/pong.png"
-                            height={250}
-                            width={406}
-                            className="card-img-top"
-                        />
-                        <div className="card-body">
-                            <h5 className="card-title">Pong</h5>
-                            <p className="card-text text-wrap">
-                                Pong clone made with Rust. <br /> This was my
-                                first real atempt of making something <br />{' '}
-                                with Rust, I thougt that making simple game
-                                could <br /> be good way to learn
-                            </p>
-                            <div className="d-flex justify-content-between">
-                                <a
-                                    href="https://github.com/idkjanevim/Pong-Rust"
-                                    className="btn btn-primary"
-                                >
-                                    Go to Github page
-                                </a>
+                    <div className="crsel">
+                        <Carousel
+                            emulateTouch={true}
+                            autoPlay={true}
+                            infiniteLoop={true}
+                            interval={5000}
+                        >
+                            <div className="card mx-5 border my-5 my-md-0">
+                                <Image
+                                    src="/imgs/pong.png"
+                                    height={250}
+                                    width={406}
+                                    className="card-img-top"
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">Pong</h5>
+                                    <p className="card-text text-wrap">
+                                        Pong clone made with Rust. <br /> This
+                                        was my first real atempt of making
+                                        something <br /> with Rust, I thougt
+                                        that making simple game could <br /> be
+                                        good way to learn
+                                    </p>
+                                    <div className="d-flex justify-content-between">
+                                        <a
+                                            href="https://github.com/idkjanevim/Pong-Rust"
+                                            className="btn btn-primary"
+                                        >
+                                            Go to Github page
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </Carousel>
                     </div>
                 </div>
             </div>
